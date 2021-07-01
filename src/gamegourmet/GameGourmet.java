e45873c68a4f129bcafd12a4141faf3c62698b4d
Emerson Santana Cunha <e.santanacunha96@gmail.com>
2021-06-30 23:46:53 -0300

    Caso de acerto e coleta da comida

--- src/gamegourmet/GameGourmet.java
+++ src/gamegourmet/GameGourmet.java
@@ -5,9 +5,11 @@
  */
 package gamegourmet;
 
+import gamegourmet.entity.Category;
 import gamegourmet.entity.Food;
 import java.util.ArrayList;
 import java.util.List;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
@@ -20,31 +22,58 @@ public class GameGourmet {
     /**
      * @param args the command line arguments
      */
+    public static final String MSG_START_GAME = "Pense em um prato que gosta";
+    public static final String PROJECT_NAME = "Game Gourmet";
+    public static final String QUESTION_FOOD_CATEGORY = "O prato que você pensou é %s";
+    public static final String CASE_SUCCESS = "Acertei de novo";
+    public static final String WHAT_FOOD_THINK = "Qual prato você pensou?";
+    public static final String WHAT_DIFERENCE_FOODS = "%s é _________, mas %s não";
+ 
     public static void main(String[] args) {
-        // TODO code application logic here
         List<Food> listFood = new ArrayList();
+        List<Category> listCategory = new ArrayList();
         
-        listFood.add(new Food("Macarrao"));
-        listFood.add(new Food("Lasanha"));
+        int startGame = JOptionPane.showConfirmDialog(null,
+                MSG_START_GAME, PROJECT_NAME, JOptionPane.DEFAULT_OPTION);
 
-       showList(listFood);
-        
-        listFood.add(new Food(showMenu()));
-        
-        showList(listFood);
+        int input = JOptionPane.showConfirmDialog(null,
+                QUESTION_FOOD_CATEGORY, PROJECT_NAME, JOptionPane.YES_NO_OPTION);
+
+        if (input == 0) {
+            int continueOK = JOptionPane.showConfirmDialog(null,
+                    CASE_SUCCESS, PROJECT_NAME, JOptionPane.DEFAULT_OPTION);
+        }
+        if (input == 1) {
+            listFood.add(new Food(showMenu()));
+            listCategory.add(new Category(howDifference()));
+        }
+
+       // showList(listFood);
     }
-    
-    public static String showMenu(){
-        String foodOption = JOptionPane.showInputDialog( " Informe qual a comida: " );
+
+    public String generateSuggestion() {
+        //pegar da lista e sugerir
+        return "teste";
+    }
+
+    public static String showMenu() {
+        String foodOption = JOptionPane.showInputDialog(WHAT_FOOD_THINK);
         return foodOption;
     }
     
-    public static void showList(List<Food> myList){
-         for (int i = 0; i < myList.size(); i++) {
+     public static String howDifference() {
+        String differenceFood = JOptionPane.showInputDialog(WHAT_DIFERENCE_FOODS);
+        return differenceFood;
+    }
+     
+     
+     
+//todo teste
+    public static void showList(List<Food> myList) {
+        for (int i = 0; i < myList.size(); i++) {
             String message = "Prato sugerido: " + myList.get(i).getDescription();
             JFrame frame = new JFrame("JOptionPane exemplo");
             JOptionPane.showMessageDialog(frame, message);
         }
     }
-
 }
---
+++ src/gamegourmet/entity/Category.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gamegourmet.entity;
+
+/**
+ *
+ * @author ermim
+ */
+public class Category {
+    private String description;
+
+    public Category(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+    
+    
+}
